57aaef4cdf5f9fde619e9a748b50951b3572a603
Raj <singhraj1553@outlook.com>
2023-06-04 14:47:07 +0530

    skillup

---
+++ projects/SkillUp/main.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+menuBtn.addEventListener("click", ()=>{
+    menuBtn.firstElementChild.classList.toggle('fa-times');
+    menu.classList.toggle("menu-open");
+});
+
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault(); //this prevents right click
+});
+document.onkeydown=function(e){
+    if(event.keycode==123){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode=="U".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode=="S".charCodeAt(0)){
+        return false;
+    }
+};
