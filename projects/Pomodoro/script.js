07c1efcd39d29306862be5a29d2e5094f9bdc3f7
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-06-23 03:46:09 +0530

    updates

--- projects/Pomodoro/script.js
+++ projects/Pomodoro/script.js
@@ -7,6 +7,9 @@ let shortBreakTime = document.getElementById('short-break-time');
 let longBreakTime = document.getElementById('long-break-time');
 let todoList = document.getElementById('todo-list');
 let newTaskInput = document.getElementById('new-task-input');
+let startButton = document.getElementById('start-btn');
+let stopButton = document.getElementById('stop-btn');
+let resetButton = document.getElementById('reset-btn');
 
 function startTimer() {
   if (!isTimerRunning) {
@@ -14,6 +17,9 @@ function startTimer() {
     seconds = 0;
     timerInterval = setInterval(updateTimer, 1000);
     isTimerRunning = true;
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
   }
 }
 
@@ -53,6 +59,9 @@ function startBreakTimer(isShortBreak) {
 function stopTimer() {
   clearInterval(timerInterval);
   isTimerRunning = false;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
 }
 
 function resetTimer() {
@@ -61,6 +70,9 @@ function resetTimer() {
   minutes = parseInt(workTime.value);
   seconds = 0;
   document.getElementById('timer').textContent = formatTime(minutes, seconds);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
 }
 
 function formatTime(minutes, seconds) {
@@ -79,8 +91,8 @@ function addTask() {
       <div class="task-actions">
         <input type="checkbox" id="${taskId}">
         <input type="text" class="task-input" value="${taskText}">
-        <button onclick="updateTask(${taskId})">Update</button>
-        <button onclick="removeTask(${taskId})">Remove</button>
+        <button onclick="updateTask(${todoList.children.length})">Update</button>
+        <button onclick="removeTask(${todoList.children.length})">Remove</button>
       </div>
     `;
     todoList.appendChild(taskItem);
@@ -88,27 +100,35 @@ function addTask() {
   }
 }
 
-function updateTask(taskId) {
-  const taskInput = document.querySelector(`#${taskId} + .task-input`);
-  if (taskInput) {
-    taskInput.disabled = !taskInput.disabled;
-    taskInput.classList.toggle('disabled');
-  }
-}
-
 function removeTask(index) {
   todoList.removeChild(todoList.children[index]);
 }
 
-
-
+function updateTask(index) {
+  const taskInput = todoList.children[index].querySelector('.task-input');
+  const taskId = 'task' + index;
+  const labelFor = taskInput.value !== '' ? taskId : '';
+  todoList.children[index].innerHTML = `
+    <div class="task-actions">
+      <input type="checkbox" id="${taskId}">
+      <input type="text" class="task-input" value="${taskInput.value}">
+      <button onclick="updateTask(${index})">Update</button>
+      <button onclick="removeTask(${index})">Remove</button>
+    </div>
+  `;
+  const label = todoList.children[index].querySelector('label');
+  if (label) {
+    label.setAttribute('for', labelFor);
+  }
+}
 
 function getCheckedTasks() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const checkedTasks = [];
-  const checkboxes = document.querySelectorAll('.task-actions input[type="checkbox"]');
   checkboxes.forEach((checkbox) => {
     if (checkbox.checked) {
-      checkedTasks.push(checkbox.id);
+      const taskIndex = parseInt(checkbox.id.replace('task', ''));
+      checkedTasks.push(taskIndex);
     }
   });
   return checkedTasks;
