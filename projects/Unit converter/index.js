30b2a46037b5301261e359c699beea234465bc66
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2023-07-15 22:21:39 +0530

    Update index.js

--- projects/Unit converter/index.js
+++ projects/Unit converter/index.js
@@ -1,19 +1,71 @@
-var feet = document.getElementById("feet");
-var inch = document.getElementById("inch");
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var option_from,option_to;
 
-feet.addEventListener("input", function () {
-  let f = this.value;
-  let i = f * 12;
+// now add listener
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
 
-  inch.value = i;
-});
+// taking initial value
+option_from = inputType.value;
+option_to   = resultType.value;
 
-inch.addEventListener("input", function () {
-  let i = this.value;
-  let f = i / 12;
 
-  if (!Number.isInteger(f)) {
-    f = f.toFixed(2);
-  }
-  feet.value = f;
-});
+function myResult(){
+
+
+// when we change the input and output type vale we need to updata the 
+// option_from and option_to
+
+	option_from = inputType.value;
+	option_to = resultType.value;
+
+
+// now compare the input and resultType value and add formula
+
+	if(option_from === "meter" && option_to==="kilometer"){
+		//this is meter to kilometer formula 
+		result.value = Number(input.value) * 0.001;
+	}else if(option_from === "meter" && option_to==="Centimeter"){
+
+		//this is meter to Centimeter formula 
+		result.value = Number(input.value) * 100;
+
+	}else if(option_from === "meter" && option_to==="meter"){
+		//this is meter to meter formula 
+		result.value = input.value
+	}
+
+
+
+	if(option_from === "kilometer" && option_to==="meter"){
+		//this is kilometer to meter formula 
+		result.value = Number(input.value) * 1000;
+	}else if(option_from === "kilometer" && option_to==="Centimeter"){
+		//this is kilometer to Centimeter formula 
+		result.value = Number(input.value) * 100000;
+	}else if(option_from === "kilometer" && option_to==="kilometer"){
+		//this is kilometer to kilometer formula 
+		result.value = input.value
+	}
+
+	if(option_from === "Centimeter" && option_to==="kilometer"){
+		//this is Centimeter to kilometer formula 
+		result.value = Number(input.value) * 0.00001;
+	}else if(option_from === "Centimeter" && option_to==="meter"){
+		//this is Centimeter to meter formula 
+		result.value = Number(input.value) * 0.01;
+	}else if(option_from === "Centimeter" && option_to==="Centimeter"){
+		//this is Centimeter to Centimeter formula 
+		result.value = input.value
+	}
+	
+
+
+
+
+
+}
