2e0df44b2bde8fb9ad5ba81d64a2bb4ade5dae51
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-08-04 13:02:37 +0530

    Added animation to scroll to top button

--- assets/to_top.js
+++ assets/to_top.js
@@ -5,7 +5,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } 
   else {
